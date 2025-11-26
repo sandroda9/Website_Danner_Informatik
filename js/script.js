592d19ea472d7fcc9eba5c355359580e2e7258ab
sandroda9 <danner.sandro@bluewin.ch>
2025-11-26 08:48:27 +0000

    Initial website structure with Bootstrap setup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Benutzerdefinierte JavaScript-Funktionen
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website geladen');
+});
+
+// Weitere JavaScript-Code hier hinzufügen
